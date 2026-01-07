151bb1f2ef2b47d5b6c4cdd2ab5e981b0ab17c60
Liubofu <luboff1992@gmail.com>
2026-01-07 18:59:40 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,90 @@
+import java.util.Arrays;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Задание 1!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int[] inputArray1 = {70, 400, 100, 250, 200};
+        int sumPay = 0; //сумма выплат
+        int maxPay = 0; //макс выплата
+        int minPay = 401; //мин выплата
+        double midPay = 0; //среднее значение
+        for (int element : inputArray1) {
+            sumPay = sumPay + element; // узнаем сумму выплат
         }
+
+        for (int max : inputArray1) {
+            if (max > maxPay) { //узнаем максимальную выплату
+                maxPay = max;
+            }
+        }
+
+        for (int i = 0; i < inputArray1.length; i++) {
+            if (inputArray1[i] < minPay) { //узнаем минимальную выплату
+                minPay = inputArray1[i];
+            }
+        }
+
+        for (double mid : inputArray1) { //узнаем среднее значение выплат
+            midPay = sumPay / inputArray1.length;
+        }
+
+        double[] outputArray1 = new double[4];
+        outputArray1[0] = sumPay;
+        outputArray1[1] = maxPay;
+        outputArray1[2] = minPay;
+        outputArray1[3] = midPay;
+
+        System.out.println(Arrays.toString(inputArray1));
+        System.out.println(Arrays.toString(outputArray1));
+
+        System.out.println("Задание 2!");
+
+        int[] inputArray2 = {35_000, 43_500, 41_000, 54_400, 39_800};
+        float[] outputArray2 = new float[5];
+        for (int i = 0; i < inputArray2.length; i++) {
+            float tax = inputArray2[i] * 0.13f;
+            outputArray2[i] = tax;
+        }
+        System.out.println(Arrays.toString(inputArray2));
+        System.out.println(Arrays.toString(outputArray2));
+
+        System.out.println("Задание 3!");
+
+        int[] inputArray3 = {5_200, 4_800, 3_400, 6_000, 5_100};
+        boolean[] outputArray3 = new boolean[5];
+        for (int i = 0; i < inputArray3.length; i++) {
+            if (inputArray3[i] > 5000) {
+                outputArray3[i] = true;
+            } else {
+                outputArray3[i] = false;
+            }
+            System.out.println(outputArray3[i]);
+        }
+        System.out.println("Задание 4!");
+
+        int[] inputArray4 = {15, 23, 48, -2, 0};
+        boolean outputArray4 = true;
+        for (int i = 0; i < inputArray4.length; i++) {
+            if (inputArray4[i] < 0) {
+                outputArray4 = false;
+                break;
+            }
+        }
+        System.out.println(Arrays.toString(inputArray4));
+        System.out.println(outputArray4);
+
+        System.out.println("Задание 5! ");
+
+        int[] inputArray5 = {100_000, 49_000, 48_500, 50_000, 35_000};
+        int outputArray5 = 0; //назначаем счетчик
+        for (int number : inputArray5) {
+            if (number > 0);
+            outputArray5++;
+        }
+        System.out.println(Arrays.toString(inputArray5));
+        System.out.println("Количество месяцев с положительной прибылью: " + outputArray5);
     }
 }
